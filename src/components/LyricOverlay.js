e4769c3eb303de3677850151ebd1b5bf0a0f5c19
unknown <106490582+lovegaoshi@users.noreply.github.com>
2023-01-19 05:59:29 -0800

    re-extract-songname

--- src/components/LyricOverlay.js
+++ src/components/LyricOverlay.js
@@ -14,7 +14,7 @@ const theme = {
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   };
 
-export const LyricOverlay = memo(function ({ showLyric, currentTime, audioName, audioId, audioCover }) {
+export const LyricOverlay = memo(function ({ showLyric, currentTime, audioName, audioId, audioCover, artist = "" }) {
     const [open, setOpen] = useState(true);
 
     useEffect(() => {
@@ -50,7 +50,7 @@ export const LyricOverlay = memo(function ({ showLyric, currentTime, audioName, 
                     >
                         <KeyboardArrowDownIcon />
                     </IconButton>
-                    <Lyric currentTime={currentTime} audioName={audioName} audioId={audioId} audioCover={audioCover} />
+                    <Lyric currentTime={currentTime} audioName={audioName} audioId={audioId} audioCover={audioCover} artist={artist} />
                 </div>
             </Dialog>
         </div>
